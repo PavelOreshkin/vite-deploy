490e621ca53b41f6454fb61b282c2cc5a161f05d
PavelOreshkin <pavel.oreshkin.1@gmail.com>
2025-08-05 22:45:24 +0400

    entry fix

--- src/app/entrypoint/App.tsx
+++ src/app/entrypoint/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { Header } from "@/shared/header";
@@ -14,9 +13,6 @@ function App() {
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>

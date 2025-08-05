16c95bfad020398c6510d4d573ce776cec110681
PavelOreshkin <pavel.oreshkin.1@gmail.com>
2025-08-05 22:37:16 +0400

    compailer plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
+// import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
 
 /**
